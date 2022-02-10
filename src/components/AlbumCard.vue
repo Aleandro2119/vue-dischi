dd20e3b98320379b8b13f84bcac58fb3a8e99396
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-10 16:32:21 +0100

    Add AlbumCard

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <img :src="poster" :alt="author" />
+      <h4>{{ title }}</h4>
+      <p>{{ author }}</p>
+      <p>{{ genre }}</p>
+      <p>{{ year }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlbumCard",
+  props: ["poster", "title", "author", "genre", "year"],
+};
+</script>
+
+<style>
+</style>
