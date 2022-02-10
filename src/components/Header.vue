1a39f2131d1765e2974606641bdf8fa4f9a52636
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-10 15:32:30 +0100

    Add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <div>
+      <img class="p-2" src="../assets/spotify.png" alt="Logo Spotify" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: #2e3a46;
+
+  img {
+    width: 5%;
+  }
+}
+</style>
